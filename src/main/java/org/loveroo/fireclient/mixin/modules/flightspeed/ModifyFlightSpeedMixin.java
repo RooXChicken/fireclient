403ae180be6b9ba4e9a6169cb691275b0044294b
rooxchicken <rooxchicken@gmail.com>
2025-08-25 05:08:23 -0400

    Fix non creative mode flight speed

--- src/main/java/org/loveroo/fireclient/mixin/modules/flightspeed/ModifyFlightSpeedMixin.java
+++ src/main/java/org/loveroo/fireclient/mixin/modules/flightspeed/ModifyFlightSpeedMixin.java
@@ -1,8 +1,10 @@
 package org.loveroo.fireclient.mixin.modules.flightspeed;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.GameMode;
 import org.loveroo.fireclient.client.FireClientside;
 import org.loveroo.fireclient.modules.FlightSpeedModule;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,6 +16,11 @@ abstract class ModifyFlightMoveSpeedMixin {
 
     @Redirect(method = "getOffGroundSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerAbilities;getFlySpeed()F"))
     private float modifySpeed(PlayerAbilities abilities) {
+        var client = MinecraftClient.getInstance();
+        if(client.player == null || client.player.getGameMode() != GameMode.CREATIVE) {
+            return abilities.getFlySpeed();
+        }
+
         var flight = (FlightSpeedModule) FireClientside.getModule("flight_speed");
         if(flight == null || !flight.getData().isEnabled()) {
             return abilities.getFlySpeed();
@@ -28,6 +35,11 @@ abstract class ModifyFlightVerticalSpeedMixin {
 
     @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerAbilities;getFlySpeed()F"))
     private float modifySpeed(PlayerAbilities abilities) {
+        var client = MinecraftClient.getInstance();
+        if(client.player == null || client.player.getGameMode() != GameMode.CREATIVE) {
+            return abilities.getFlySpeed();
+        }
+
         var flight = (FlightSpeedModule) FireClientside.getModule("flight_speed");
         if(flight == null || !flight.getData().isEnabled()) {
             return abilities.getFlySpeed();
