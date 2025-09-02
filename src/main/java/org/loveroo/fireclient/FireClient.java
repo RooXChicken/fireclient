f08e3ee5d63c3f5afdce14ea1ab575da1025034f
rooxchicken <rooxchicken@gmail.com>
2025-09-02 08:55:49 -0400

    Fix indicator bug and add prevent deleting favorited kits

--- src/main/java/org/loveroo/fireclient/FireClient.java
+++ src/main/java/org/loveroo/fireclient/FireClient.java
@@ -12,6 +12,7 @@ public class FireClient implements ModInitializer {
     }
 
     public static final String MOD_ID = "fireclient";
+    public static final String FIRECLIENT_VERSION = "1.3.2";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static final String KEYBIND_CATEGORY = "key.category.fireclient";
