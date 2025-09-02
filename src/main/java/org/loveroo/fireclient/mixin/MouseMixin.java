8ce02675d377cb7cd92d4dd7a6605aebdf83cefc
rooxchicken <rooxchicken@gmail.com>
2025-09-02 10:26:02 -0400

    Add more keys and mouse support to keybinds

--- src/main/java/org/loveroo/fireclient/mixin/MouseMixin.java
+++ src/main/java/org/loveroo/fireclient/mixin/MouseMixin.java
@@ -2,8 +2,12 @@ package org.loveroo.fireclient.mixin;
 
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Mouse;
+
+import org.loveroo.fireclient.FireClient;
 import org.loveroo.fireclient.client.FireClientside;
 import org.loveroo.fireclient.keybind.Key.KeyType;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,16 +15,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Keyboard.class)
-public abstract class KeyboardMixin {
+@Mixin(Mouse.class)
+public abstract class MouseMixin {
 
     @Shadow @Final
     private MinecraftClient client;
 
-    @Inject(method = "onKey", at = @At("HEAD"), cancellable = true)
-    private void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo info) {
+    @Inject(method = "onMouseButton", at = @At("HEAD"), cancellable = true)
+    private void onKey(long window, int button, int action, int mods, CallbackInfo info) {
         if (window == client.getWindow().getHandle()) {
-            var status = FireClientside.getKeybindManager().onKey(KeyType.KEY_CODE, key, scancode, action, modifiers);
+            var key = GLFW.GLFW_MOUSE_BUTTON_1 + button;
+            var status = FireClientside.getKeybindManager().onKey(KeyType.MOUSE, key, -1, action, mods);
 
             if(!status) {
                 info.cancel();
