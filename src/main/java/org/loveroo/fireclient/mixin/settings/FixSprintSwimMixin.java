a4ee2d859183bcb1108d538b4976947c6b3e962c
rooxchicken <rooxchicken@gmail.com>
2025-09-04 14:32:17 -0400

    Disable sprint swin on Minemen Club

--- src/main/java/org/loveroo/fireclient/mixin/settings/FixSprintSwimMixin.java
+++ src/main/java/org/loveroo/fireclient/mixin/settings/FixSprintSwimMixin.java
@@ -1,11 +1,14 @@
 package org.loveroo.fireclient.mixin.settings;
 
+import java.util.Set;
+
 import org.loveroo.fireclient.FireClient;
 import org.loveroo.fireclient.RooHelper;
 import org.loveroo.fireclient.client.FireClientside;
 import org.loveroo.fireclient.data.FireClientOption;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,6 +26,11 @@ import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 @Mixin(ClientPlayerInteractionManager.class)
 abstract class FixSprintSwimMixin {
     
+    @Unique
+    private static final Set<String> minemenClubBrands = Set.of(
+        "XeBungee", "ClubSpigot", "ClubPaper"
+    );
+
     @Inject(method = "attackEntity", at = @At("TAIL"))
     private void resetSprint(PlayerEntity player, Entity target, CallbackInfo info) {
         var client = MinecraftClient.getInstance();
@@ -30,9 +38,21 @@ abstract class FixSprintSwimMixin {
             return;
         }
 
+        if(onMinemen()) {
+            return;
+        }
+
         client.player.setSprinting(false);
         var sprintInvoker = (SprintPacketAccessor)(ClientPlayerEntity)client.player;
         sprintInvoker.sendSprintingPacketInvoker();
+    }
+
+    @Unique
+    private boolean onMinemen() {
+        var brand = RooHelper.getServerBrand();
+        var isMMCBrand = minemenClubBrands.stream().anyMatch((mmcBrand) -> mmcBrand.equalsIgnoreCase(brand));
+
+        return (isMMCBrand || RooHelper.getIp().toLowerCase().endsWith("minemen.club"));
     }
 }
 
