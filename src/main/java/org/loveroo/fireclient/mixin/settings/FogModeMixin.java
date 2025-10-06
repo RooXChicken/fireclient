e59eb91f6997e797426f5e7dd218e71b94ed3792
rooxchicken <rooxchicken@gmail.com>
2025-10-06 15:38:03 -0400

    1.21.4 fixes

--- src/main/java/org/loveroo/fireclient/mixin/settings/FogModeMixin.java
+++ src/main/java/org/loveroo/fireclient/mixin/settings/FogModeMixin.java
@@ -2,6 +2,9 @@ package org.loveroo.fireclient.mixin.settings;
 
 import java.util.Map;
 
+import net.minecraft.client.render.BackgroundRenderer;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.Fog;
 import org.joml.Vector4f;
 import org.loveroo.fireclient.FireClient;
 import org.loveroo.fireclient.client.FireClientside;
@@ -12,6 +15,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -25,7 +29,6 @@ import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.TropicalFishEntityRenderer;
 import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.render.entity.state.TropicalFishEntityRenderState;
-import net.minecraft.client.render.fog.FogRenderer;
 import net.minecraft.client.util.SkinTextures;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
@@ -95,71 +98,39 @@ abstract class FogFishMixin {
             return;
         }
 
-        tropicalFishEntityRenderState.variety = TropicalFishEntity.Pattern.SNOOPER;
+        tropicalFishEntityRenderState.variety = TropicalFishEntity.Variety.SNOOPER;
         tropicalFishEntityRenderState.baseColor = DyeColor.GRAY.getEntityColor();
         tropicalFishEntityRenderState.patternColor = DyeColor.RED.getEntityColor();
     }
 }
 
-@Mixin(FogRenderer.class)
+@Mixin(WorldRenderer.class)
 abstract class FogAmplifierMixin {
 
-    @ModifyVariable(method = "applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-    private Vector4f modifyFogColor(Vector4f original) {
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/BackgroundRenderer;getFogColor(Lnet/minecraft/client/render/Camera;FLnet/minecraft/client/world/ClientWorld;IF)Lorg/joml/Vector4f;"))
+    private Vector4f modifyFogColor(Camera camera, float tickDelta, ClientWorld world, int clampedViewDistance, float skyDarkness) {
         if(!fogModeEnabled()) {
-            return original;
+            return BackgroundRenderer.getFogColor(camera, tickDelta, world, clampedViewDistance, skyDarkness);
         }
 
         return new Vector4f(1.0f, 0.0f, 0.0f, 0.8f);
     }
 
-    @ModifyVariable(method = "applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V", at = @At("HEAD"), ordinal = 0, argsOnly = true)
-    private float modifyEnvironmentStart(float original) {
-        if(!fogModeEnabled()) {
-            return original;
-        }
-
-        return 8.0f;
-    }
-
-    @ModifyVariable(method = "applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V", at = @At("HEAD"), ordinal = 1, argsOnly = true)
-    private float modifyEnvironmentEnd(float original) {
-        if(!fogModeEnabled()) {
-            return original;
-        }
-
-        return 32.0f;
-    }
-
-    @ModifyVariable(method = "applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V", at = @At("HEAD"), ordinal = 2, argsOnly = true)
-    private float modifyRenderStart(float original) {
-        if(!fogModeEnabled()) {
-            return original;
-        }
-
-        return 8.0f;
-    }
-
-    @ModifyVariable(method = "applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V", at = @At("HEAD"), ordinal = 3, argsOnly = true)
-    private float modifyRenderEnd(float original) {
-        if(!fogModeEnabled()) {
-            return original;
-        }
-
-        return 32.0f;
-    }
-
-    @ModifyVariable(method = "applyFog(Ljava/nio/ByteBuffer;ILorg/joml/Vector4f;FFFFFF)V", at = @At("HEAD"), ordinal = 4, argsOnly = true)
-    private float modifySkyEnd(float original) {
-        if(!fogModeEnabled()) {
-            return original;
-        }
-
-        return 128f;
-    }
-
     private boolean fogModeEnabled() {
         return (FireClientside.getSetting(FireClientOption.FOG_MODE) == 1 && MinecraftClient.getInstance().world != null);
+    }
+}
+
+@Mixin(BackgroundRenderer.class)
+abstract class CloserFogMixin {
+
+    @ModifyVariable(method = "getFogColor", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private static int modifyEnvironmentStart(int original) {
+        if(!(FireClientside.getSetting(FireClientOption.FOG_MODE) == 1 && MinecraftClient.getInstance().world != null)) {
+            return original;
+        }
+
+        return 8;
     }
 }
 
@@ -176,15 +147,15 @@ abstract class FogCloudColorMixin {
     }
 }
 
-@Mixin(WorldRenderer.class)
-abstract class FogSkyColorMixin {
-
-    @ModifyVariable(method = "render", at = @At("HEAD"), ordinal = 0)
-    private Vector4f modifyFogColor(Vector4f original) {
-        if(FireClientside.getSetting(FireClientOption.FOG_MODE) == 0) {
-            return original;
-        }
-
-        return new Vector4f(1.0f, 0.0f, 0.0f, 0.8f);
-    }
-}
+//@Mixin(WorldRenderer.class)
+//abstract class FogSkyColorMixin {
+//
+//    @ModifyVariable(method = "render", at = @At("HEAD"), ordinal = 0)
+//    private Vector4f modifyFogColor(Vector4f original) {
+//        if(FireClientside.getSetting(FireClientOption.FOG_MODE) == 0) {
+//            return original;
+//        }
+//
+//        return new Vector4f(1.0f, 0.0f, 0.0f, 0.8f);
+//    }
+//}
--- src/main/java/org/loveroo/fireclient/mixin/settings/ScreenshotTextMixin.java
+++ src/main/java/org/loveroo/fireclient/mixin/settings/ScreenshotTextMixin.java
@@ -36,30 +36,30 @@ public abstract class ScreenshotTextMixin {
     @Unique
     private static final Color color2 = Color.fromRGB(0x82D96C);
 
-    @ModifyVariable(method = "saveScreenshot(Ljava/io/File;Ljava/lang/String;Lnet/minecraft/client/gl/Framebuffer;ILjava/util/function/Consumer;)V", at = @At("HEAD"), argsOnly = true, ordinal = 0)
-    private static Consumer<Text> modifyMessage(Consumer<Text> original, @Local(ordinal = 0, argsOnly = true) File file) {
-        if(FireClientside.getSetting(FireClientOption.EXTRA_SCREENSHOT_OPTIONS) == 0) {
-            return original;
-        }
-
-        return (message) -> {
-            var screenshotFile = getScreenshotFilename(file);
-            var copyText = RooHelper.gradientText(Text.translatable("fireclient.settings.extra_screenshot_options.copy.message").getString(), color1, color2);
-
-            // TODO: completely borked
-            var clipboard = "ERROR";
-            try {
-                clipboard = Files.readString(Paths.get(screenshotFile.toURI()));
-            }
-            catch(Exception e) {}
-
-            var click = new ClickEvent.CopyToClipboard(clipboard);
-            var hover = new HoverEvent.ShowText(Text.translatable("fireclient.settings.extra_screenshot_options.copy.hover", screenshotFile.getName()));
-
-            var copyClickable = copyText.setStyle(copyText.getStyle().withClickEvent(click).withHoverEvent(hover));
-
-            var newMessage = message.copy().append(" ").append(copyClickable);
-            original.accept(newMessage);
-        };
-    }
+//    @ModifyVariable(method = "saveScreenshot(Ljava/io/File;Ljava/lang/String;Lnet/minecraft/client/gl/Framebuffer;ILjava/util/function/Consumer;)V", at = @At("HEAD"), argsOnly = true, ordinal = 0)
+//    private static Consumer<Text> modifyMessage(Consumer<Text> original, @Local(ordinal = 0, argsOnly = true) File file) {
+//        if(FireClientside.getSetting(FireClientOption.EXTRA_SCREENSHOT_OPTIONS) == 0) {
+//            return original;
+//        }
+//
+//        return (message) -> {
+//            var screenshotFile = getScreenshotFilename(file);
+//            var copyText = RooHelper.gradientText(Text.translatable("fireclient.settings.extra_screenshot_options.copy.message").getString(), color1, color2);
+//
+//            // TODO: completely borked
+//            var clipboard = "ERROR";
+//            try {
+//                clipboard = Files.readString(Paths.get(screenshotFile.toURI()));
+//            }
+//            catch(Exception e) {}
+//
+//            var click = new ClickEvent.CopyToClipboard(clipboard);
+//            var hover = new HoverEvent.ShowText(Text.translatable("fireclient.settings.extra_screenshot_options.copy.hover", screenshotFile.getName()));
+//
+//            var copyClickable = copyText.setStyle(copyText.getStyle().withClickEvent(click).withHoverEvent(hover));
+//
+//            var newMessage = message.copy().append(" ").append(copyClickable);
+//            original.accept(newMessage);
+//        };
+//    }
 }
